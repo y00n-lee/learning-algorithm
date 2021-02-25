6727bccbda02e54a311db55e5235612cfe6cbf91
y00n-lee <0613myday@gmail.com>
2021-02-25 20:35:59 +0900

    피보나치 수

---
+++ PS/baekjoon/Python 배우기(51 - 100)/p_2747.py
@@ -0,0 +1,28 @@
+# 피보나치 수 - 백준 2747번
+
+#DP 사용 방법
+def Fibonacci(x):
+    dp = [0,1]
+    for i in range(2, 46):
+        dp.append(dp[i-1] + dp[i-2])
+
+    return dp[x]
+
+
+n = int(input())
+print(Fibonacci(n))
+
+
+# #재귀함수 사용 방법 - 시간 초과
+# def Fibonacci(x):
+#     if x == 0:
+#         return 0
+#
+#     if x == 1:
+#         return 1
+#
+#     if x >= 2 :
+#         return Fibonacci(x-1) + Fibonacci(x-2)
+#
+# n = int(input())
+# print(Fibonacci(n))
