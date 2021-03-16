492f387a3efb93daf4c187fdd10cdd3f5eb11df1
y00n-lee <0613myday@gmail.com>
2021-03-16 23:57:56 +0900

    백설 공주와 일곱 난쟁이 solved on Mar 16,2021

---
+++ PS/baekjoon/Python 배우기(101 - 150)/b3040_백설공주와일곱난쟁이.py
@@ -0,0 +1,23 @@
+height = []
+
+for _ in range(9):
+    height.append(int(input()))
+
+value = sum(height) - 100
+for i in range(len(height)-1):
+    one = height[i]
+    flag = False
+    for j in range(i+1, len(height)):
+        two = height[j]
+
+        if (one + two) == value:
+            height.remove(one)
+            height.remove(two)
+            flag = True
+            break
+
+    if flag:
+        break
+
+for v in height:
+    print(v)
