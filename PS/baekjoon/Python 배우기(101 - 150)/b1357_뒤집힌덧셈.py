f4f03530c4b7faa308f597a02109fa080cdcbffe
y00n-lee <0613myday@gmail.com>
2021-03-25 23:38:31 +0900

    뒤집힌 덧셈 solved on Mar 25,2021

---
+++ PS/baekjoon/Python 배우기(101 - 150)/b1357_뒤집힌덧셈.py
@@ -0,0 +1,11 @@
+def rev(n):
+    n = str(n)
+    ans = ''
+    for i in range(1, len(n)+1):
+        ans += n[-i]
+
+    return int(ans)
+
+
+a, b = map(int, input().split())
+print(rev(rev(a)+rev(b)))
