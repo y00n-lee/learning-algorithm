918d7e65f43dc00965b4e1ed322397ed5c656af2
y00n-lee <0613myday@gmail.com>
2021-03-25 23:14:50 +0900

    짝수를 찾아라 solved on Mar 25,2021

---
+++ PS/baekjoon/Python 배우기(101 - 150)/b3058_짝수를찾아라.py
@@ -0,0 +1,9 @@
+for _ in range(int(input())):
+    num = list(map(int, input().split()))
+    eval_num = []
+
+    for i in num:
+        if i % 2 == 0:
+            eval_num.append(i)
+
+    print(sum(eval_num), min(eval_num))
